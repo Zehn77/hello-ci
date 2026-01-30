80276d2618aa56add49831102eb8739679800126
Zehn77 <zekhn777@gmail.com>
2026-01-30 18:05:27 +0500

    f

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get("/", (req, res) => {
   res.send("OK - Cool");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("App is listening on port: ", process.env.PORT);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("App is listening on port: ", PORT);
 });
